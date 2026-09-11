29297418e0e67af9a4641a5d31c3304eb76d90b5
Матвій Ніколйчук <matvijniko@gmail.com>
2026-09-11 09:38:12 +0300

    1 laba

---
+++ script2.py
@@ -0,0 +1,28 @@
+print("Hello World!")
+
+name ="Matviy"
+age = 16
+height = 1.82
+numbers = [1, 2, 3]
+letters = ["a", "b", "c"]
+student = {"name": "Matviy", "age": 16, "height": 1.82}
+ist_student = True
+
+print(name, type(name))
+print(age, type(age))
+print(height, type(height))
+print(numbers, type(numbers))
+print(letters, type(letters))
+print(student, type(student))
+
+a = 10
+b = 20
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+#коментар
